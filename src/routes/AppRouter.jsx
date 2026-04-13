b5b1a9fdc4f1b584c8d326881d7fbeff5bc9409f
ibrohim3 <ibrohim.2128382@gmail.com>
2026-04-13 22:28:13 +0500

    d

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Calendar from "../pages/calendar/Calendar";
 import Dashboard from "../pages/dashboard/Dashboard";
-// import AddProjects from "../pages/AddProjects/AddProjects";
 import AddNewProject from "../pages/Projects/AddNewProject";
 import ProjectsCollect from "../pages/Projects/ProjectsCollect";
 import ProjectsMainPage from "../pages/AddProjects/ProjectsMainPage";
 import MainVac from "../pages/Vacations/MainVac";
 import MyProfile from "../pages/MyProfile/MyProfile";
-import Employees from "../pages/Employees/Employees";
 import InfoPortalMain from "../pages/Info/InfoPortalMain";
-// import NotificationToggle from "../pages/MyProfile/Notif/NotificationToggle";
+import { Loader } from "lucide-react";
 
+const Calendar = lazy(() => import("../pages/calendar/Calendar"));
+const Employees = lazy(() => import("../pages/Employees/Employees"));
 function AppRouter() {
   return (
     <div>
@@ -20,8 +19,29 @@ function AppRouter() {
         <Route path="/main-page" element={<ProjectsMainPage />} />
         <Route path="/projects/*" element={<ProjectsCollect />} />
         <Route path="/projects/newProject" element={<AddNewProject />} />
-        <Route path="/employes" element={<Employees />} />
-        <Route path="/calendar" element={<Calendar />} />
+
+        <Route
+          path="/employes"
+          element={
+            <Suspense
+              fallback={<Loader className="animate-spin mx-auto mt-10" />}
+            >
+              <Employees />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/calendar"
+          element={
+            <Suspense
+              fallback={<Loader className="animate-spin mx-auto mt-10" />}
+            >
+              <Calendar />
+            </Suspense>
+          }
+        />
+
         <Route path="/vacations/*" element={<MainVac />} />
         <Route path="/info-portal/*" element={<InfoPortalMain />} />
         <Route path="/my-profile/*" element={<MyProfile />} />
